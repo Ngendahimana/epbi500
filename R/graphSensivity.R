3061056c06006ec5902f35094fab835cd9b2f407
Ngendahimana <dkn18@case.edu>
2017-03-07 11:19:06 -0500

    added sensitivity function

--- R/graphSensivity.R
+++ R/graphSensivity.R
@@ -22,3 +22,19 @@ add <- function(x, y) {
 subtract <- function(x, y) {
   x - y
 }
+
+#' Visualizing sensivity results
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' subtract(1, 1)
+#' subtract (10, 1)
+
+graphSensitivity <- function(x, y) {
+  x - y
+}
+
+
+
